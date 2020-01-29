1a95c725611da6c1e915a1b32345fa5721e6aca4
Carlos Estrada <carlos.deg02@hotmail.com>
2020-01-29 08:23:11 -0600

    Finished parte 2 and 3

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -4,11 +4,78 @@ let mongoose = require( 'mongoose' );
 let jsonParser = bodyParser.json();
 let { DATABASE_URL, PORT } = require( './config' );
 
+let {BookmarkController} = require('./model');
 let app = express();
 
 let server;
 
-/* Tu código va aquí */
+app.put('/api/bookmarks/:id', jsonParser, (req, res) => {
+	if (req.body.id == undefined) {
+		res.statusMessage = "ID no proporcionado en el cuerpo";
+		return res.status(406).send();
+	}
+
+	if (req.body.id !== req.params.id) {
+		res.statusMessage = "ID de parametro y cuerpo no coinciden";
+		return res.status(409).send();
+	}
+
+	let id = req.params.id;
+
+	let titulo, desc, url;
+
+	titulo = req.body.titulo;
+	desc = req.body.descripcion;
+	url = req.body.url;
+
+	if (titulo == undefined && desc == undefined && url == undefined) {
+		res.statusMessage = "No hay parametros a actualizar";
+		return res.status(406).send();
+	}
+
+	let nuevoBookmark = {};
+
+	if (titulo != undefined) {
+		nuevoBookmark.titulo = titulo;
+	}
+
+	if (desc != undefined) {
+		nuevoBookmark.descripcion = desc;
+	}
+
+	if (url != undefined) {
+		nuevoBookmark.url = url;
+	}
+
+	BookmarkController.update(id, nuevoBookmark)
+		.then(nb => {
+			if (nb == null) {
+				res.statusMessage = "Bookmark no encontrado";
+				return res.status(404).send();
+			}
+			// Por alguna razón, mongoose nos está regresando el bookmark anterior,
+			// antes de que se actualice, por lo tanto, para regresar el bookmark
+			// actualizado vamos a obtenerlo fresco de la BD
+			console.log(nb);
+
+			BookmarkController.getById(id)
+				.then(b => {
+					console.log(b)
+					return res.status(202).json(b);
+				})
+				.catch(error => {
+					console.log(error);
+					res.statusMessage = "Error con la BD";
+					return res.status(500).send();
+				});
+		})
+		.catch(error => {
+			console.log(error);
+			res.statusMessage = "Error con la BD";
+			return res.status(500).send();
+		});
+
+});
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -3,8 +3,40 @@ let uuid = require( 'uuid' );
 
 mongoose.Promise = global.Promise;
 
-/* Tu código va aquí */
+let bookmarkCollection = mongoose.Schema({
+    // Guardamos el id como tipo string ya que así esta guardado en los ejemplos 
+    // proporcionados y facilita las queries a la DB por medio del ID.
+    // Este ID va a coexistir con el _id que genera mongo ya que ese es para uso
+    // interno de mongo y este es para nuestro uso particular
+    id: String,
+    titulo: String,
+    descripcion: String,
+    url: String,
+});
+
+let Bookmark = mongoose.model('bookmarks', bookmarkCollection);
+
+let BookmarkController = {
+    getById: function(id) {
+        return Bookmark.findOne({id: id})
+            .then(b => {
+                return b;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    },
+    update: function(id, nuevo) {
+        return Bookmark.findOneAndUpdate({id: id}, nuevo)
+            .then(ub => {
+                return ub;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    }
+}
 
 module.exports = {
-    
+    BookmarkController
 };
